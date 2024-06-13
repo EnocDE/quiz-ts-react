39a05001f3a6f8cee196f04f543dd2e06db7b221
Enoc Dominguez <enocescazan@hotmail.com>
2024-06-12 19:09:05 -0600

    add persist storage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@mui/material"
 import useQuestionData from "../hooks/useQuestionData"
+import { useQuestionStore } from "../store/questions"
 
 export default function Footer() {
   const { correct, incorrect, unaswered } = useQuestionData()
+  const reset = useQuestionStore(state => state.reset)
 
   return (
     <footer style={{ marginTop: '20px', display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Button variant="text" onClick={() => reset()}>Reiniciar</Button>
       <strong>Respuestas correctas: {correct}</strong>
       <strong>Respuestas incorrectas: {incorrect}</strong>
       <strong>No contestadas: {unaswered}</strong>
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,7 +1,7 @@
-import { create } from "zustand";
-import { type Question } from "../types";
-import { devtools } from "zustand/middleware";
 import confetti from "canvas-confetti";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { type Question } from "../types";
 
 interface State {
   questions: Question[];
@@ -10,63 +10,77 @@ interface State {
   selectAnswer: (questionId: number, aswerIndex: number) => void;
   goNextQuestion: () => void;
   goPrevQuestion: () => void;
+  reset: () => void;
 }
 
 export const useQuestionStore = create<State>()(
-  devtools((set, get) => ({
-    questions: [],
-    currentQuestion: 0, // posición del array de questions
+  persist(
+    (set, get) => ({
+      questions: [],
+      currentQuestion: 0, // posición del array de questions
 
-    fetchQuestion: async (limit: number) => {
-      const response = await fetch(`http://localhost:5173/data.json`);
-      const result = await response.json();
-      const questions = result.sort(() => Math.random() - 0.5).slice(0, limit);
-      set({
-        questions,
-      });
-    },
-    selectAnswer: (questionId, answerIndex) => {
-      const { questions } = get();
-      const newQuestions = structuredClone(questions);
-
-      const questionIndex = newQuestions.findIndex(
-        (question) => question.id === questionId
-      );
-      const questionInfo = newQuestions[questionIndex];
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
-
-      if (questionInfo?.userSelectedAnswer !== undefined) return;
-      if (isCorrectUserAnswer) confetti();
-
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex,
-      };
-
-      set({
-        questions: newQuestions,
-      });
-    },
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const nextQuestion = currentQuestion + 1;
-
-      if (nextQuestion < questions.length) {
+      fetchQuestion: async (limit: number) => {
+        const response = await fetch(`http://localhost:5173/data.json`);
+        const result = await response.json();
+        const questions = result
+          .sort(() => Math.random() - 0.5)
+          .slice(0, limit);
         set({
-          currentQuestion: nextQuestion,
+          questions,
         });
-      }
-    },
-    goPrevQuestion: () => {
-      const { currentQuestion } = get();
-      const prevQuestion = currentQuestion - 1;
+      },
+      selectAnswer: (questionId, answerIndex) => {
+        const { questions } = get();
+        const newQuestions = structuredClone(questions);
 
-      if (prevQuestion >= 0) {
+        const questionIndex = newQuestions.findIndex(
+          (question) => question.id === questionId
+        );
+        const questionInfo = newQuestions[questionIndex];
+        const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+
+        if (questionInfo?.userSelectedAnswer !== undefined) return;
+        if (isCorrectUserAnswer) confetti();
+
+        newQuestions[questionIndex] = {
+          ...questionInfo,
+          isCorrectUserAnswer,
+          userSelectedAnswer: answerIndex,
+        };
+
         set({
-          currentQuestion: prevQuestion,
+          questions: newQuestions,
         });
-      }
-    },
-  }))
+      },
+      goNextQuestion: () => {
+        const { currentQuestion, questions } = get();
+        const nextQuestion = currentQuestion + 1;
+
+        if (nextQuestion < questions.length) {
+          set({
+            currentQuestion: nextQuestion,
+          });
+        }
+      },
+      goPrevQuestion: () => {
+        const { currentQuestion } = get();
+        const prevQuestion = currentQuestion - 1;
+
+        if (prevQuestion >= 0) {
+          set({
+            currentQuestion: prevQuestion,
+          });
+        }
+      },
+      reset: () => {
+        set({
+          currentQuestion: 0,
+          questions: [],
+        });
+      },
+    }),
+    {
+      name: "questions",
+    }
+  )
 );
